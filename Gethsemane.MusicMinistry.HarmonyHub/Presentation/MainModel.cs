1b46a280fc5a12d74299048a4221b536684295b9
Arvin Kenneth C. Alvarez <rvnlvrz@gmail.com>
2024-12-20 22:03:48 +0800

    Add initial inventory management page

--- Gethsemane.MusicMinistry.HarmonyHub/Presentation/MainModel.cs
+++ Gethsemane.MusicMinistry.HarmonyHub/Presentation/MainModel.cs
@@ -3,7 +3,7 @@ namespace Gethsemane.MusicMinistry.HarmonyHub.Presentation;
 public partial record MainModel
 {
     private INavigator _navigator;
-
+    
     public MainModel(
         IOptions<AppConfig> appInfo,
         INavigator navigator)
@@ -16,18 +16,6 @@ public partial record MainModel
     public string? Title { get; }
 
     public IState<string> Name => State<string>.Value(this, () => string.Empty);
-
-    public IState<int> Count => State<int>.Value(this, () => 0);
-
-    public IFeed<string> CounterText => Count.Select(_currentCount => _currentCount switch
-    {
-        0 => "Press Me",
-        1 => "Pressed Once!",
-        _ => $"Pressed {_currentCount} times!"
-    });
-
-    public async Task Counter(CancellationToken ct) =>
-        await Count.Update(x => ++x, ct);
 
     public async Task GoToSecond()
     {
